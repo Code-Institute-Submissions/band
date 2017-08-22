b091d02bb163f0ec2b06752e767cab5d0d05b7d3
James Sanderson <jamessandersoon@gmail.com>
2017-08-22 13:22:25 +0100

    Changes to program - git had older version

--- js/controller.js
+++ js/controller.js
@@ -23,59 +23,9 @@ angular.module('RouteControllers', [])
 		   })
 
 
-		   //google maps
-
-	    $('#marker1').click(function() {
-   			$('#map1').modal('show');
-	   	})
-	   	$('#map1').on('shown.bs.modal', function () {
-	    	var currentCenter = map1.getCenter(); //get center
-		    google.maps.event.trigger(map1, "resize");
-		    map1.setCenter(currentCenter); //Re-set center
-		})
-
-	   	$('#marker2').click(function() {
-	   		$('#map2').modal('show');
-	   	})
-	   	$('#map2').on('shown.bs.modal', function () {
-	    	var currentCenter = map2.getCenter(); //get center
-		    google.maps.event.trigger(map2, "resize");
-		    map2.setCenter(currentCenter); //Re-set center
-		})
-
-	   	$('#marker3').click(function() {
-	   		$('#map3').modal('show');
-	   	})
-	   	$('#map3').on('shown.bs.modal', function () {
-    	var currentCenter = map3.getCenter(); //get center
-	    google.maps.event.trigger(map3, "resize");
-	    map3.setCenter(currentCenter); //Re-set center
-		})
-
-	  	$('#marker4').click(function() {
-	   		$('#map4').modal('show');
-	   	})
-	    $('#map4').on('shown.bs.modal', function () {
-    	var currentCenter = map4.getCenter(); //get center
-	    google.maps.event.trigger(map4, "resize");
-	    map4.setCenter(currentCenter); //Re-set center
-		})
 
 
-	   	$('#marker5').click(function() {
-	   		$('#map5').modal('show');
-	   	})
-	    $('#map5').on('shown.bs.modal', function () {
-    	var currentCenter = map5.getCenter(); //get center
-	    google.maps.event.trigger(map5, "resize");
-	    map5.setCenter(currentCenter); //Re-set center
-		});
-
-
-
-
-
-		//newsletetr sign up modal
+		//newsletter sign up modal
 	   if (Cookies.get('newsletteronce') == null) {
 			Cookies.set ('newsletteronce', 'yes', {expires: 7});
 			setTimeout(function(){
@@ -134,31 +84,18 @@ angular.module('RouteControllers', [])
    		 });
 
 
-
-		angular.element(document).ready(function() {
-			$scope.songs = [{song:'Babel', link: 'https://geo.itunes.apple.com/us/album/little-lion-man/id983362067?i=983362074&mt=1&app=music'},
-                         {song:'I will wait', link: 'https://geo.itunes.apple.com/us/album/i-will-wait/id547449573?i=547449577&mt=1&app=music'},
-                         {song:'Choose Me', link: 'https://geo.itunes.apple.com/us/album/choose-me/id1110609098?i=1110609281&mt=1&app=music'},
-                         {song:'Let it go', link: 'https://geo.itunes.apple.com/us/album/let-it-go/id948749743?i=948749751&mt=1&app=music'},
-                         {song:'Till the end', link: 'https://geo.itunes.apple.com/us/album/till-the-end/id1105707158?i=1105707681&mt=1&app=music'},
-                         {song:'We are free', link: 'https://geo.itunes.apple.com/us/album/we-are-free-live/id821513143?i=821513159&mt=1&app=music'}]
-		$('#songsearch').keypress(function() {
-			$('#songs').show();
-			})
-		});
-
-
 	})
 
 
-	.controller('BookingsForm', function($scope) {
+	.controller('BookingsForm', function($scope, $http) {
 		angular.element(document).ready(function() {	
 			$(function(){
 				$( "#datepicker" ).datepicker({
 				  dateFormat: 'dd MM yy',
-				  beforeShowDay: checkBadDates
+				  beforeShowDay: checkBadDates,
+				  minDate: 0
 				  })
-				var $myBadDates = ["10 October 2017","21 November 2017","12 December 2017","13 January 2017","14 February 2017","15 March 2017"];
+				var $myBadDates = ["10 October 2017","21 November 2017","12 December 2017","13 January 2017","14 February 2017","15 March 2017","22 September 2017","04 September 2017","18 September 2017"];
 
 				function checkBadDates(mydate){
 				var $return=true;
@@ -178,7 +115,7 @@ angular.module('RouteControllers', [])
 
 
 
-		$scope.registrationUser = {}; //create new object to store the date
+		$scope.registrationUser = {}; //create new object to store the data
 
 		$scope.submitForm  = function() { //link to the form name
 			if ($scope.registrationForm.$valid) {
@@ -187,8 +124,17 @@ angular.module('RouteControllers', [])
                 $scope.registrationUser.email = $scope.email;
                 $scope.registrationUser.date = $scope.date;
             }
-            console.log($scope.registrationUser.fname + " " + $scope.registrationUser.lname + " " + $scope.registrationUser.email + " " + $scope.registrationUser.date)
-		}
+        }
+
+		$scope.submitForm=function(){
+        /* while compiling form , angular created this object*/
+        var data=$scope.fields;  
+        /* post to server*/
+        $http.post("js/bookings.json", data); 
+        console.log(data);
+    	}
+
+	
 
 	})
 
